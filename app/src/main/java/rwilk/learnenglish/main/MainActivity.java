1d67948c8b3e1888413c8f2ee9fcfadd9375f02e
RWilk94 <wilkrafal15@gmail.com>
2018-02-15 21:40:03 +0100

    Create ViewPager and BottomNavigationView.

--- app/src/main/java/rwilk/learnenglish/main/MainActivity.java
+++ app/src/main/java/rwilk/learnenglish/main/MainActivity.java
@@ -1,15 +1,39 @@
 package rwilk.learnenglish.main;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
 import rwilk.learnenglish.R;
+import rwilk.learnenglish.utils.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeView();
+    }
+
+    private void initializeView(){
+        mAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mAdapter.addFragment(new DashboardFragment());
+        mAdapter.addFragment(new ProfileFragment());
+
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mAdapter);
+        mViewPager.setOffscreenPageLimit(2);
+
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        bottomNavigationViewEx.inflateMenu(R.menu.bottom_navigation_menu);
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.setupWithViewPager(mViewPager, true);
     }
 }
